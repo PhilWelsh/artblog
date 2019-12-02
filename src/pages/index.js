8c561fd9e0d7436521bfa448ada02dc56e00f5df
Phil Welsh <philwelsh3d@gmail.com>
2019-12-02 14:32:31 -0500

    adding contentful remark

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,40 @@
-import React from "react"
+import React from 'react';
+import { graphql } from 'gatsby';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-  </Layout>
-)
+	<Layout>
+		<SEO title="Home" />
+		<section>
+			<h1>Hi people</h1>
+			<p>Welcome to your new Gatsby site.</p>
+			<p>Now go build something great.</p>
+			<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+				<Image />
+			</div>
+		</section>
+		<section style={{ background: 'black' }} />
+	</Layout>
+);
 
-export default IndexPage
+export default IndexPage;
+
+export const query = graphql`
+	query HomePage {
+		contentfulHomePage {
+			title
+			date
+			content {
+				content
+			}
+			image {
+				file {
+					url
+				}
+			}
+		}
+	}
+`;
